e254e138f81dd5a8be9617ed928ccb002fba0ed3
mdaminz <aminz.muhammad110000011@gmail.com>
2025-04-10 02:52:11 +0800

    fix heroku database

--- config/database.php
+++ config/database.php
@@ -85,18 +85,20 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => $DATABASE_URL['host'],
-            'port' => $DATABASE_URL['port'],
-            'database' => ltrim($DATABASE_URL['path'], "/"),
-            'username' => $DATABASE_URL['user'],
-            'password' => $DATABASE_URL['pass'],
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
+        'driver' => 'pgsql',
+        'url' => env('DB_URL'),
+        'host' => parse_url(env('DATABASE_URL'), PHP_URL_HOST),
+        'port' => parse_url(env('DATABASE_URL'), PHP_URL_PORT),
+        'database' => ltrim(parse_url(env('DATABASE_URL'), PHP_URL_PATH), '/'),
+        'username' => parse_url(env('DATABASE_URL'), PHP_URL_USER),
+        'password' => parse_url(env('DATABASE_URL'), PHP_URL_PASS),
+        'charset' => env('DB_CHARSET', 'utf8'),
+        'prefix' => '',
+        'prefix_indexes' => true,
+        'search_path' => 'public',
+        'sslmode' => 'require',  // For Heroku, you often need to set this to 'require' for SSL connections
+],
+
         ],
 
         'sqlsrv' => [
